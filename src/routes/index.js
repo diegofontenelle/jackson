48b3179d4e7cce624349f9be81d13ffd015dc1f1
Diego Fontenelle <diego.lima.fontenelle@gmail.com>
2020-03-26 04:37:20 -0300

    refactor(Routes): add new routes and change root

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import Home from '../pages/Home'
+import Login from '../pages/Login'
 
 // import { Container } from './styles';
 
+const browserHistory = createBrowserHistory()
+
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={Home} />
-    </BrowserRouter>
+    <Router history={browserHistory}>
+      <Route path="/" exact component={Login} />
+      <Route path="/home" exact component={Home} />
+    </Router>
   )
 }
